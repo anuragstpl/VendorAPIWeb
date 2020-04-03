c70e6b1f23e9b26b6bac7b0de383cc960b367e62
Irfan Siddiqui <irfan.siddiqui@srmtechsol.com>
2020-04-04 00:18:05 +0530

    changes with new pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,16 @@
-﻿using DataLayer.DataHelper;
+﻿using ClosedXML.Excel;
+using DataLayer.DataHelper;
 using EntityLayer;
+using LinqToExcel;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using VendorAPI.Models;
-using ClosedXML.Excel;
-using System.Data;
-using System.IO;
 
 namespace VendorAPI.Controllers
 {
@@ -25,10 +27,20 @@ namespace VendorAPI.Controllers
         public ActionResult Index(UserEntity userEntity)
         {
             string viewName = "";
-            bool isUserValid = userHelper.LoginUser(userEntity);
-            if (isUserValid)
+            userEntity = userHelper.LoginUser(userEntity);
+
+            if (userEntity.UserID > 0)
             {
-                viewName = "Dashboard";
+                Session["UserID"] = userEntity.UserID;
+                FormsAuthentication.SetAuthCookie(userEntity.Username, true);
+                if (userEntity.UserRoleID == 1)
+                {
+                    viewName = "AdminDashboard";
+                }
+                else
+                {
+                    viewName = "Dashboard";
+                }
             }
             else
             {
@@ -43,9 +55,9 @@ namespace VendorAPI.Controllers
         {
             DataTable dt = new DataTable("ServiceProviders");
             dt.Columns.AddRange(new DataColumn[8] { new DataColumn("Name"),
-new DataColumn("Status"),
-new DataColumn("GoLiveDate"),
-new DataColumn("ProjectManager"),
+            new DataColumn("Status"),
+            new DataColumn("GoLiveDate"),
+            new DataColumn("ProjectManager"),
             new DataColumn("Phase"),
             new DataColumn("Fees"),
             new DataColumn("Type"),
@@ -55,7 +67,7 @@ new DataColumn("ProjectManager"),
 
             foreach (var serviceProvider in serviceProviders)
             {
-                dt.Rows.Add(serviceProvider.ProviderName, serviceProvider.Status, serviceProvider.GoLiveDate, serviceProvider.ProjectManager, serviceProvider.Phase, serviceProvider.Fees, serviceProvider.Type, serviceProvider.Update);
+                dt.Rows.Add(serviceProvider.Name, serviceProvider.Status, serviceProvider.GoLiveDate, serviceProvider.ProjectManager, serviceProvider.Phase, serviceProvider.Fees, serviceProvider.Type, serviceProvider.Update);
             }
 
             using (XLWorkbook wb = new XLWorkbook())
@@ -69,6 +81,7 @@ new DataColumn("ProjectManager"),
             }
         }
 
+        [Authorize]
         public ActionResult DashBoard()
         {
             DashboardData dashboardData = serviceProviderHelper.GetDashboardData();
@@ -79,7 +92,121 @@ new DataColumn("ProjectManager"),
             ViewBag.QAGoLive = dashboardData.QAGoLive;
             ViewBag.SPBilled = dashboardData.SPBilled;
             List<ServiceProviderEntity> serviceProviders = serviceProviderHelper.GetServiceProviders();
+
             return View(serviceProviders);
+        }
+
+        [Authorize]
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult UploadConfirmation()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateProfile()
+        {
+            return RedirectToAction("UpdateUserProfile");
+        }
+
+        public ActionResult UpdateUserProfile()
+        {
+            UserProfileData userProfileData = new UserProfileData();
+
+            userProfileData = userHelper.GetUserProfileData(Convert.ToInt32(Session["UserID"]));
+            return View(userProfileData);
+        }
+
+
+        public ActionResult UpdateUserProfilePost(UserProfileData userProfileData)
+        {
+            try
+            {
+                userProfileData.UserId = Convert.ToInt32(Session["UserID"]);
+                userHelper.UpdateUser(userProfileData);
+                TempData["Message"] = "Profile updated successfuly.";
+            }
+            catch (Exception ex) { ViewBag.message = "some issue occured. please try again"; }
+            return RedirectToAction("UpdateUserProfile", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult UploadExcel(HttpPostedFileBase FileUpload)
+        {
+            ServiceProviderHelper serviceProviderHelper = new ServiceProviderHelper();
+            ServiceProviderEntity serviceProviderEntity = null;
+
+            string viewName = "";
+            try
+            {
+                if (FileUpload != null)
+                {
+                    if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    {
+                        string filename = FileUpload.FileName;
+
+                        if (filename.EndsWith(".xlsx") || filename.EndsWith(".xls"))
+                        {
+                            string targetpath = Server.MapPath("~/UploadExcel/");
+                            FileUpload.SaveAs(targetpath + filename);
+                            string pathToExcelFile = targetpath + filename;
+
+                            string sheetName = "Sheet1";
+
+                            var excelFile = new ExcelQueryFactory(pathToExcelFile);//
+                            var providerDetails = from a in excelFile.Worksheet<ServiceProviderEntity>(sheetName) select a;
+
+                            foreach (var provider in providerDetails)
+                            {
+                                if (provider.Name != null)
+                                {
+                                    DateTime? GoLiveDate = null;
+                                    GoLiveDate = Convert.ToDateTime(provider.GoLiveDate);
+
+                                    serviceProviderEntity = new ServiceProviderEntity();
+
+                                    serviceProviderEntity.Name = provider.Name;
+                                    serviceProviderEntity.Status = provider.Status;
+                                    serviceProviderEntity.GoLiveDate = provider.GoLiveDate;
+                                    serviceProviderEntity.ProjectManager = provider.ProjectManager;
+                                    serviceProviderEntity.Phase = provider.Phase;
+                                    serviceProviderEntity.Fees = provider.Fees;
+                                    serviceProviderEntity.Type = provider.Type;
+                                    serviceProviderEntity.Update = provider.Update;
+
+                                    serviceProviderHelper.AddProvider(serviceProviderEntity);
+
+                                    EmailSetting emailSetting = new EmailSetting();
+                                    string Body = System.IO.File.ReadAllText(System.Web.Hosting.HostingEnvironment.MapPath("~/EmailTemplates/ExcelUploadMail.html"));
+
+                                    bool isMailSend = emailSetting.SendMail("irfan.siddiqui@srmtechsol.com", "", Body);
+                                    viewName = "uploadconfirmation";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ViewBag.Message = "Only Excel file format is allowed";
+                            viewName = "AdminDashboard";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Only Excel file format is allowed";
+                        viewName = "AdminDashboard";
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Please select file.";
+                    return RedirectToAction("AdminDashboard", "Home");
+                }
+            }
+            catch (Exception ex) { ViewBag.message = "some issue occured. please try again"; }
+            return RedirectToAction(viewName);
         }
 
         public ActionResult About()
@@ -94,6 +221,12 @@ new DataColumn("ProjectManager"),
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Models/EmailSetting.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace VendorAPI.Models
+{
+    public class EmailSetting
+    {
+        public string SMTPServer = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["SMTPServer"]);
+        public string SMTPUserName = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["SMTPUserName"]);
+        public string SMTPPassword = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["SMTPPassword"]);
+        public string SMTPPort = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["SMTPPort"]);
+        public string EmailFrom = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["EmailFrom"]);
+        public string Subject = Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["Subject"]);
+
+        public bool SendMail(string To, string Cc, string Body)
+        {
+            bool status = false;
+            try
+            {
+                MailMessage mm = new MailMessage(EmailFrom, To);
+                mm.Subject = Subject;
+                mm.Body = Body;
+                mm.IsBodyHtml = true;
+
+                if (!string.IsNullOrEmpty(Cc))
+                    mm.CC.Add(Cc);
+
+                //if (!string.IsNullOrEmpty(Bcc))
+                //    mm.Bcc.Add(Bcc);
+
+                mm.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = SMTPServer;
+                smtp.UseDefaultCredentials = false;
+
+                System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                NetworkCred.UserName = SMTPUserName;
+                NetworkCred.Password = SMTPPassword;
+                smtp.Credentials = NetworkCred;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtp.Port = Convert.ToInt32(SMTPPort);
+                //if (smtpAtt != null)
+                //{
+                //    mm.Attachments.Add(smtpAtt);
+                //}
+                try
+                {
+                    smtp.Send(mm);
+                    status = true;
+                }
+                catch (Exception ex)
+                {                    
+                    status = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;                
+            }
+            return status;
+        }
+    }
+}
